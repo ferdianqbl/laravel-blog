1c2fb678a7f8c038fc8e08efc9d6ae777ffec57c
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-07-20 23:15:35 +0700

    register validation

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -8,7 +8,7 @@
     <div class="col-lg-4">
       @if (session()->has('success'))
       <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}, {{session('username')}}
+        {{ session('success') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
       @endif
